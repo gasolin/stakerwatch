a426d846afc552a2a8210d686cb7569ccd075fde
Fred Lin <gasolin@gmail.com>
2021-03-14 20:25:11 +0800

    fix

--- src/utils.js
+++ src/utils.js
@@ -3,9 +3,9 @@
  * @param {boolean} returnArray return as string or array
  */
 export const parseArg = (param) =>
-  param && typeof param === 'string' && param.indexOf(',') > 0
-    ? param.split(',').map(item => item.trim())
-    : param;
+  param && typeof param === 'string' && param.indexOf(',') > 0 ?
+    param.split(',').map((item) => item.trim()) :
+    param;
 
 /**
  * convert array or string to array
@@ -19,22 +19,23 @@ export const toArray = (data) => Array.isArray(data) ? data : [data];
 export const getConfig = (env, defaultValue) => {
   const param = process && process.env[`SAIHUBOT_${env}`];
   const data = parseArg(param);
-  return data
-    ? data
-    : defaultValue;
-}
+  return data ?
+    data :
+    defaultValue;
+};
 
 /** get random item from an array. */
-export const getRandomItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+export const getRandomItem = (arr, defaultItem = '') => arr.length > 0 ?
+  arr[Math.floor(Math.random() * arr.length)] : defaultItem;
 
-export const isAddr = data => data.length === 42;
+export const isAddr = (data) => data.startsWith('0x') && data.length === 42;
 
 export const singleAddr = (data) => Array.isArray(data) ? data[0] : data;
 
 export const addrTxSearch = (target, addrfunc, txfunc) => {
-  if(isAddr(target)) {
+  if (isAddr(target)) {
     addrfunc && addrfunc(target);
   } else {
     txfunc && txfunc(target);
   }
-}
+};
