7d4d11557433e256186d6f66a767decd01b4b9ad
Fred Lin <gasolin@gmail.com>
2021-03-14 17:11:19 +0800

    web ui

---
+++ src/defi/abi.js
@@ -0,0 +1,3 @@
+// DEFI SDK
+// https://docs.zerion.io/supported-protocols/read-only-adapters
+export const CONTRACT_ADDRESS = '0x06fe76b2f432fdfecaef1a7d4f6c3d41b5861672';
---
+++ src/helpers/useCoingeckoPrices.js
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from 'react';
+
+export const useCoingeckoPrices = (addresses, fetch) => {
+  const [tokens, setTokens] = useState({});
+
+
+
+  return [tokens]
+}
+
+export default useCoingeckoPrices;
